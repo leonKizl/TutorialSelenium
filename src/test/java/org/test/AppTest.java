04b30da286c0ddf090e2a54bc8f6ff2f4024aa4c
Leon Kozłowski <103368148+leonKizl@users.noreply.github.com>
2024-11-16 15:11:25 +0100

    first commit

---
+++ src/test/java/org/test/AppTest.java
@@ -0,0 +1,22 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
---
+++ src/test/java/org/test/tutorTest.java
@@ -0,0 +1,10 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class tutorTest {
+    @Test
+    public void someTest(){
+        System.out.println("Test running");
+    }
+}

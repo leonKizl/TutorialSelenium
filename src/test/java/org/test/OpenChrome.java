becd3dc9a91e5f137f7a490f492e35ed95111dde
Leon Kozłowski <103368148+leonKizl@users.noreply.github.com>
2024-11-17 18:59:09 +0100

    Set up chrome browser. Adding Chrome Driver, adjusting Chromeoptions

---
+++ src/test/java/org/test/OpenChrome.java
@@ -0,0 +1,28 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class OpenChrome {
+
+    @Test
+    public void openBrowser() {
+        // Устанавливаем путь к драйверу Chrome
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\leoni\\OneDrive\\Рабочий стол\\instalators\\drivers\\chromedriver-win64\\chromedriver.exe");
+
+        // Инициализация опций для Chrome
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        // Инициализация драйвера с опциями
+        WebDriver driverChrome = new ChromeDriver(options);
+
+        // Открываем сайт (например, форум)
+        driverChrome.get("https://google.com");
+
+        // Закрываем браузер
+//        driverChrome.quit();
+    }
+}

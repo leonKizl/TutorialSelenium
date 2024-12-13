40f771c0994a49c034d350557268f9fce6164e4a
Leon Kozłowski <103368148+leonKizl@users.noreply.github.com>
2024-12-13 17:40:56 +0100

    file upload

---
+++ src/test/java/org/test/UploadTest.java
@@ -0,0 +1,14 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class UploadTest {
+    @Test
+    public void uploadTest(){
+        WebDriver driver = DriverSelector.getDriver("chrome");
+        driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
+        driver.findElement(By.cssSelector("#myFile")).sendKeys("C:\\Users\\leoni\\OneDrive\\Рабочий стол\\DSC_0319.NEF");
+    }
+}

0f84c3fd1ed8a71bbc79033ca7f98e127cd3f64a
Leon Kozłowski <103368148+leonKizl@users.noreply.github.com>
2024-12-17 21:35:11 +0100

    gitignore and upload

--- src/test/java/org/test/UploadTest.java
+++ src/test/java/org/test/UploadTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 public class UploadTest {
     @Test
     public void uploadTest(){
+        //Remember it
         WebDriver driver = DriverSelector.getDriver("chrome");
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
         driver.findElement(By.cssSelector("#myFile")).sendKeys("C:\\Users\\leoni\\OneDrive\\Рабочий стол\\DSC_0319.NEF");
